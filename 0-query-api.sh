ebf0e0c95dde351a2773f7beeaf5f334567bdf90
philmikejones <philmikejones@gmail.com>
2015-09-24 10:57:47 +0100

    improved readme

--- 0-query-api.sh
+++ 0-query-api.sh
@@ -1,46 +1,44 @@
-#!/bin/bash
+#! /bin/bash
 
 # 1. Replace 'insert_api_key_here' with your api key
-# 2. Batch input_xxxx.csv files in to rows of 2,500 (or a factor of 2,500)
+# 2. Batch directions in to csv files in rows of 2,500 (or a factor of 2,500)
 #    You can use more if you enable billing
-# 3. Place input_xxxx.csv files in Scheduled/
-# 4. Dropbox stops to prevent syncing errors while file is still being written
-# 5. Batch calls into days (2,500 calls per day; 2,500 rows per file)
-# 6. Construct URL as per Google Maps Directions API documentation:
+# 3. Place input csv files in Scheduled/
+# 4. Construct URL as per Google Maps Directions API documentation:
 #    https://developers.google.com/maps/documentation/directions/intro
-# 7. Quotas reset midnight PST so set sleep delays accordingly
-# 8. JSON output written to json/
+#    Key option is the transit mode
+# 5. Quotas reset midnight PST so set sleep delays accordingly
+# 6. JSON output written to json/. Set appropriate file names
+# 7. If writing to a dropbox synced folder I strongly recommend stopping
+#    dropbox while writing. This can be achieved with 'dropbox stop' and
+#    'dropbox start' once the file is written.
 
 # Day 1
-dropbox stop
-sed 1d Scheduled/input_one_a.csv |  # 'skips' the first row containing headers
-while IFS=, read student origin destination O_lat O_long D_lat D_long
+# dropbox stop  # uncomment if writing to dropbox synced folder
+sed 1d Scheduled/example.csv |  # 'skips' the first row containing headers
+while IFS=, read id origin destination O_lat O_long D_lat D_long
 do
-	curl "https://maps.googleapis.com/maps/api/directions/json?origin=$O_lat,$O_long&destination=$D_lat,$D_long&mode=transit&transit_routing_preference=fewer_transfers&arrival_time=1441612800&key=insert_api_key_here";
+	curl "https://maps.googleapis.com/maps/api/directions/json?origin=$O_lat,$O_long&destination=$D_lat,$D_long&mode=transit&key=insert_api_key_here";
 	sleep 0.5  # 2 calls per second
-done > json/output_one_a.json
-dropbox start
+done > json/example.json
+# dropbox start  # resume syncing once file fully written
 
-sleep 84600  # About 24 hours. Set this to start just after midnight PST
+# sleep 84600  # About 24 hours. Set this to resume just after midnight PST
 
-# Day 2
-dropbox stop
-sed 1d Scheduled/input_two.csv |
-while IFS=, read student origin destination O_lat O_long D_lat D_long
-do
-	curl "https://maps.googleapis.com/maps/api/directions/json?origin=$O_lat,$O_long&destination=$D_lat,$D_long&mode=transit&transit_routing_preference=fewer_transfers&arrival_time=1441612800&key=insert_api_key_here";
-	sleep 0.5
-done > json/output_three.json
-dropbox start
-
-sleep 84600  # About 24 hours
-
-# Day 3
-dropbox stop
-sed 1d Scheduled/input_five.csv |
-while IFS=, read student origin destination O_lat O_long D_lat D_long
-do
-	curl "https://maps.googleapis.com/maps/api/directions/json?origin=$O_lat,$O_long&destination=$D_lat,$D_long&mode=transit&transit_routing_preference=fewer_transfers&arrival_time=1441612800&key=insert_api_key_here";
-	sleep 0.5
-done > json/output_five.json
-dropbox start
+# # Day 2
+# sed 1d Scheduled/day_two.csv |
+# while IFS=, read id origin destination O_lat O_long D_lat D_long
+# do
+# 	curl "https://maps.googleapis.com/maps/api/directions/json?origin=$O_lat,$O_long&destination=$D_lat,$D_long&mode=transit&key=insert_api_key_here";
+# 	sleep 0.5
+# done > json/day_two.json
+#
+# sleep 84600  # About 24 hours
+#
+# # Day 3
+# sed 1d Scheduled/day_three.csv |
+# while IFS=, read id origin destination O_lat O_long D_lat D_long
+# do
+# 	curl "https://maps.googleapis.com/maps/api/directions/json?origin=$O_lat,$O_long&destination=$D_lat,$D_long&mode=transit&key=insert_api_key_here";
+# 	sleep 0.5
+# done > json/day_three.json
--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Query Goolge Maps Directions API
 
 ## Purpose
-This repository sends queries to the Google Maps Directions API and returns directions results in json format. There are some example `jq` filters to parse the json results. The scripts use bash so you need a *nix machine with bash or [cygwin](https://www.cygwin.com/) for Windows.
+This repository sends queries to the Google Maps Directions API and returns directions results in `json` format. There are some example `jq` filters to parse the `json` results. The scripts use bash so you need a \*nix machine with bash or [cygwin](https://www.cygwin.com/) for Windows.
 
 ## Instructions
 1. You need a Google Maps Directions API key. Instructions from [Google Developers](https://developers.google.com/maps/documentation/directions/get-api-key).
 2. Add your key to the file `0-query-api.sh` where instructed.
 3. It's best if you can geocode (i.e. convert postcodes/address to long/lat co-ordinates) your origin and destinations beforehand. Options to do this include [GeoConvert](http://geoconvert.mimas.ac.uk/) (now free, UK only) and Google's own [Maps Geocoding API](https://developers.google.com/maps/documentation/geocoding/intro).
-4. Use the coordinates to create a unique hyperlink for each origin/destination pair you want to query and save these one per line in a text file. Use *nix line endings.
-5. Make `0-query-api.sh` executable (`chmod u+x 0-query-api.sh`) and run it. I strongly suggest testing with just a few cases first to get a feel for what's happening and check for errors; you only get 2,500 queries/day with a free account.
+4. Enter your coordinates into a csv file saved in Scheduled/. An example is provided in the correct format. Make sure your csv file uses \*nix line endings. The easiest way to do this in Windows is to use [LibreOffice Calc](https://www.libreoffice.org/download/libreoffice-fresh/) rather than Microsoft Excel.
+5. Make `0-query-api.sh` executable (`chmod u+x 0-query-api.sh`) and run it (`./0-query-api.sh`). I strongly suggest testing with just a few cases first - or use the example provided - to get a feel for what's happening and check for errors; you only get 2,500 queries/day with a free account.
 6. The results are returned in `json` format which you can read as plain text and optionally parse with `jq`.
 
 ## Compliance with Google Terms of Service
