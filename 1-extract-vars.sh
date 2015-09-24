ba37fc5a2a172a4fa10c203673604b674271b542
philmikejones <philmikejones@gmail.com>
2015-09-24 11:04:53 +0100

    changes to extract vars example

--- 1-extract-vars.sh
+++ 1-extract-vars.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 # Uses jq to parse output json files and extract required values
-# It's provided as an example only
 # Outputs written to Cleaned/
+# Example below extracts key information about overall route
 
-jq .routes[].legs[].start_address       json/output_one_a.json > Cleaned/one_a_start_address.txt &
-jq .routes[].legs[].end_address         json/output_one_a.json > Cleaned/one_a_end_address.txt   &
-jq .routes[].legs[].departure_time.text json/output_one_a.json > Cleaned/one_a_depart_time.txt   &
-jq .routes[].legs[].arrival_time.text   json/output_one_a.json > Cleaned/one_a_arrival_time.txt  &
-jq .routes[].legs[].distance.text       json/output_one_a.json > Cleaned/one_a_distance.txt      &
-jq .routes[].legs[].duration.text       json/output_one_a.json > Cleaned/one_a_duration.txt
+jq .routes[].legs[].start_address       json/example.json > Cleaned/example_start_address.txt &
+jq .routes[].legs[].end_address         json/example.json > Cleaned/example_end_address.txt   &
+jq .routes[].legs[].departure_time.text json/example.json > Cleaned/example_depart_time.txt   &
+jq .routes[].legs[].arrival_time.text   json/example.json > Cleaned/example_arrival_time.txt  &
+jq .routes[].legs[].distance.text       json/example.json > Cleaned/example_distance.txt      &
+jq .routes[].legs[].duration.text       json/example.json > Cleaned/example_duration.txt
